791d84acd479183ca88310592ce47807d6aedef4
Mustafa Dag <mustafaa_dag@hotmail.com>
2020-07-03 23:59:50 -0400

    working on it

--- script.js
+++ script.js
@@ -6,3 +6,11 @@ currentTime = currentTime.startOf("hour");
 
 var pastTime = moment().startOf('day').add(9, "hours");
 console.log(currentTime);
+
+var timeBlock1 = pastTime.add(0, "h");
+timeBlock1 = timeBlock1.format('hh:mm A');
+$(".block1").text(timeBlock1);
+
+var timeBlock2 = pastTime.add(1, "h");
+timeBlock2 = timeBlock2.format('hh:mm A');
+$(".block2").text(timeBlock2);
