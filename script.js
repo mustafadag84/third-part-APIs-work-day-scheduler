10b18a378feb831bd0b683e85677b87a42943135
Mustafa Dag <mustafaa_dag@hotmail.com>
2020-07-05 12:01:55 -0400

    working on it

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// use the Moment.js library to work with date and time
 $("#currentDay").text(moment().format('dddd MMMM Do YYYY, h:mm a'));
 
 var currentTime = moment();
@@ -7,6 +8,7 @@ currentTime = currentTime.startOf("hour");
 var pastTime = moment().startOf('day').add(9, "hours");
 console.log(currentTime);
 
+// all my variables goes here
 var timeBlock1 = pastTime.add(0, "h");
 timeBlock1 = timeBlock1.format('hh:mm A');
 $(".block1").text(timeBlock1);
@@ -14,3 +16,31 @@ $(".block1").text(timeBlock1);
 var timeBlock2 = pastTime.add(1, "h");
 timeBlock2 = timeBlock2.format('hh:mm A');
 $(".block2").text(timeBlock2);
+
+var timeBlock3 = pastTime.add(1, "h");
+timeBlock3 = timeBlock3.format('hh:mm A');
+$(".block3").text(timeBlock3);
+
+var timeBlock4 = pastTime.add(1, "h");
+timeBlock4 = timeBlock4.format('hh:mm A');
+$(".block4").text(timeBlock4);
+
+var timeBlock5 = pastTime.add(1, "h");
+timeBlock5 = timeBlock5.format('hh:mm A');
+$(".block5").text(timeBlock5);
+
+var timeBlock6 = pastTime.add(1, "h");
+timeBlock6 = timeBlock6.format('hh:mm A');
+$(".block6").text(timeBlock6);
+
+var timeBlock7 = pastTime.add(1, "h");
+timeBlock7 = timeBlock7.format('hh:mm A');
+$(".block7").text(timeBlock7);
+
+var timeBlock8 = pastTime.add(1, "h");
+timeBlock8 = timeBlock8.format('hh:mm A');
+$(".block8").text(timeBlock8);
+
+var timeBlock9 = pastTime.add(1, "h");
+timeBlock9 = timeBlock9.format('hh:mm A');
+$(".block9").text(timeBlock9);
